be8b27f0447e195f78c47efc0a37a6c326807473
Jakub Kuritka <jakub.kuritka@gmail.com>
2023-12-19 14:53:22 +0100

    blsaj

--- main.ts
+++ main.ts
@@ -1,14 +1,39 @@
-OLED.init(128, 64)
+function sendData () {
+    radio.sendValue("movement", movement)
+    radio.sendValue("noise", noise)
+    serial.writeValue("movement", movement)
+    serial.writeValue("noise", noise)
+    movement = 0
+    noise = Environment.ReadNoise(AnalogPin.P2)
+}
+function movementDetected () {
+    movement = movement + 1
+    noise = (noise + Environment.ReadNoise(AnalogPin.P2)) / 2
+}
+let noise = 0
+let movement = 0
+led.enable(true)
+led.setBrightness(255)
+radio.setGroup(2)
+radio.setTransmitPower(4)
+movement = 0
+noise = Environment.ReadNoise(AnalogPin.P2)
 basic.forever(function () {
-    OLED.clear()
-    OLED.writeString("Vlhkost pudy: ")
-    OLED.writeNum(Environment.ReadSoilHumidity(AnalogPin.P1))
-    OLED.writeStringNewLine("%")
-    OLED.writeString("Vlhkost vzduchu: ")
-    OLED.writeNum(Environment.octopus_BME280(Environment.BME280_state.BME280_humidity))
-    OLED.writeStringNewLine("%")
-    OLED.writeString("Teplota: ")
-    OLED.writeNum(Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C))
-    OLED.writeStringNewLine("C")
-    basic.pause(5000)
+    if (tinkercademy.PIR(DigitalPin.P1)) {
+        movementDetected()
+    } else {
+    	
+    }
+    led.plotBarGraph(
+    Math.min(100, Environment.ReadNoise(AnalogPin.P2)),
+    100,
+    true
+    )
+    basic.pause(1000)
+})
+control.inBackground(function () {
+    while (true) {
+        sendData()
+        basic.pause(5000)
+    }
 })
