3b74bc4344e022f856bd3953b3413cd9c022e4f2
Jakub Kuritka <jakub.kuritka@gmail.com>
2024-02-16 09:54:04 +0100

    send data every second

--- main.ts
+++ main.ts
@@ -1,18 +1,17 @@
-led.enable(false)
+led.enable(true)
 radio.setGroup(2)
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    if (Environment.PIR(DigitalPin.P1) || Environment.ReadNoise(AnalogPin.P2) >= 40) {
-        if (Environment.PIR(DigitalPin.P1)) {
-            radio.sendValue("m", 1)
-        } else {
-            radio.sendValue("m", 0)
-        }
-        radio.sendValue("n", Environment.ReadNoise(AnalogPin.P2))
-        radio.sendValue("ll", input.lightLevel())
-        radio.sendValue("t", input.temperature())
-        radio.sendValue("mf", input.magneticForce(Dimension.X))
-        basic.pause(1000)
+    if (Environment.PIR(DigitalPin.P1)) {
+        radio.sendValue("m", 1)
+        basic.showIcon(IconNames.Chessboard)
     } else {
-        basic.pause(2000)
+        radio.sendValue("m", 0)
+        basic.clearScreen()
     }
+    radio.sendValue("n", Environment.ReadNoise(AnalogPin.P2))
+    radio.sendValue("ll", input.lightLevel())
+    radio.sendValue("t", input.temperature())
+    radio.sendValue("mf", input.magneticForce(Dimension.X))
+    basic.pause(1000)
 })
