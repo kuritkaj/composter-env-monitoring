dd5e4e9c6779d6ce3359afdf830cb9879f29e755
Jakub Kuritka <jakub.kuritka@gmail.com>
2024-03-14 08:12:06 +0100

    ddd

--- main.ts
+++ main.ts
@@ -1,25 +1,32 @@
 function updateMovement (num: number) {
     if (movement != num) {
         movement = num
-        radio.setTransmitPower(7)
-        radio.sendValue("m", movement)
+        radio.sendValue("m", num)
         if (movement == 1) {
             basic.showIcon(IconNames.Chessboard)
         } else {
-            basic.showIcon(IconNames.SmallDiamond)
+            basic.clearScreen()
         }
     }
-    radio.setTransmitPower(4)
 }
+radio.onReceivedString(function (receivedString) {
+    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    if (receivedString == "checkSignal") {
+        radio.sendValue("signalStrength", signal)
+    }
+})
+let signal = 0
 let movement = 0
+movement = 0
+radio.setTransmitPower(7)
 radio.setGroup(2)
 basic.showIcon(IconNames.Yes)
-movement = 0
+power.lowPowerEnable(LowPowerEnable.Prevent)
 basic.forever(function () {
     if (Environment.PIR(DigitalPin.P1)) {
         updateMovement(1)
     } else {
         updateMovement(0)
     }
-    basic.pause(1000)
+    basic.pause(500)
 })
